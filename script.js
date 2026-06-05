bd9a3badcc6ee0dad3df0f1a7b7283ec23a092d7
ashifzeeya097-Dev <ashifzeeya0@gmail.com>
2026-06-05 16:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const hr = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+function displayTime() {
+const date = new Date();
+
+
+const hh = date.getHours();
+const mm = date.getMinutes();
+const ss = date.getSeconds();
+
+const hRotation = 30 * (hh % 12) + mm / 2;
+const mRotation = 6 * mm + ss / 10;
+const sRotation = 6 * ss;
+
+hr.style.transform = `rotate(${hRotation}deg)`;
+min.style.transform = `rotate(${mRotation}deg)`;
+sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+
+displayTime();
+setInterval(displayTime, 1000);
+
+const themeContainer = document.getElementById("themeContainer");
+
+function changeTheme(theme) {
+themeContainer.className = `theme-container ${theme}`;
+}
+
+document.getElementById("darkBtn").addEventListener("click", () => {
+changeTheme("dark");
+});
+
+document.getElementById("blueBtn").addEventListener("click", () => {
+changeTheme("blue");
+});
+
+document.getElementById("greenBtn").addEventListener("click", () => {
+changeTheme("green");
+});
+const clock = document.getElementById("clock");
+
+function changeTheme(theme) {
+    clock.className = `clock ${theme}`;
+}
